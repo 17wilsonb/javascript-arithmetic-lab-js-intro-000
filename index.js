aea93a4b35122dd6782e510020a078a415803615
Brett Wilson <wilson.3458@osu.edu>
2020-05-04 07:05:17 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,16 @@
+// Adds inputs:
+function add(a,b){
+  return a + b;
+}
+// Subtracts b from a:
+function subtract(a,b){
+  return a - b;
+}
+// Divides a by b:
+function divide(a,b){
+  return a / b;
+}
+// Multiplies inputs:
+function multiply(a,b){
+  return a * b;
+}
